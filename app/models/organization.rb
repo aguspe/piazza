355a09d89410631ee3aafb4945f49f7abfa1b936
aguspe <agustin.pe94@gmail.com>
2024-01-30 21:32:58 +0100

    Add tests for the user model

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,5 @@
 class Organization < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  has_many :memberships, dependent: :destroy
+  has_many :members, through: :memberships, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
+  has_many :memberships, dependent: :destroy
+  has_many :organizations, through: :memberships
+  before_validation :strip_name_and_email
+
+  private
+
+  def strip_name_and_email
+    self.name = self.name&.strip
+    self.email = self.email&.strip
+  end
 end
