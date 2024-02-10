54e1f8baf2f74613ec13284084bf439204e0edc1
aguspe <agustin.pe94@gmail.com>
2024-02-10 22:52:18 +0100

    Test added for session create

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:jerry)
+  end
+
+  test 'the user logs in with the right credentials' do
+    assert_difference('@user.app_sessions.count', 1) do
+      post login_path, params: { user: { email: @user.email, password: 'password' } }
+
+      assert_redirected_to root_path
+    end
+  end
+
+  test 'the user fails to log in with the wrong credentials' do
+    assert_no_difference('@user.app_sessions.count') do
+      post login_path, params: { user: { email: @user.email, password: 'wrong_password' } }
+
+      assert_select '.notification', I18n.t('sessions.create.failure')
+    end
+  end
 end
